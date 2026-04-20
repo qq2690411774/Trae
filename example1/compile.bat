9be5406ca6e4ea38320c3dbfff358bf725dfd57a
qq2690411774 <64956901+qq2690411774@users.noreply.github.com>
2026-04-20 09:52:49 +0800

    Delete example1 directory

--- example1/compile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-g++ excel_sql.cpp -o excel_sql.exe
-echo Compilation completed!
-dir excel_sql.exe
--- example1/test_min.cpp
+++
@@ -1,6 +0,0 @@
-#include <windows.h>
-
-int main() {
-    MessageBox(NULL, "编译环境测试", "成功", MB_OK);
-    return 0;
-}
